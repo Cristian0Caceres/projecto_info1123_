8269076f00f2b256b0d723c480ca787059d597bd
Cristian0Caceres <134634536+Cristian0Caceres@users.noreply.github.com>
2023-11-10 17:34:40 -0300

    herencia de clases de los organismos y sus metodos

--- python.py
+++ python.py
@@ -25,13 +25,46 @@ class organismo:
            #especies estan presentes cerca de si y son del genero opuesto se reproduscan creando otro
            #ser de la misma especie pero con los atrivutos reducidos por unos 3 ciclos aproximadamente
            #ademas de que por esos ciclos no se pueda reproducir y tanpoco sus padres
-class depredador_01 (organismo):
+class carnivoro (organismo):
+    def __init__(self, vida, daño, energia, sed, movimiento, estado, genero, posicion, contador_de_caza):
+        super().__init__(vida, daño, energia, sed, movimiento, estado, genero, posicion)
+        self.cdc=contador_de_caza
+    def inanicion_desidratacion(self):
+        return super().inanicion_desidratacion()
+    def death(self):
+        return super().death()
+    def reproduction(self):
+        return super().reproduction()
     pass
-class hervivoro_01 (organismo):
+class hervivoro (organismo):
+    def __init__(self, vida, daño, energia, sed, movimiento, estado, genero, posicion):
+        super().__init__(vida, daño, energia, sed, movimiento, estado, genero, posicion)
+    def inanicion_desidratacion(self):
+        return super().inanicion_desidratacion()
+    def death(self):
+        return super().death()
+    def reproduction(self):
+        return super().reproduction()
     pass
-class omnivoro_01 (organismo):
+class omnivoro (organismo):
+    def __init__(self, vida, daño, energia, sed, movimiento, estado, genero, posicion, cotador_de_caza):
+        super().__init__(vida, daño, energia, sed, movimiento, estado, genero, posicion)
+        self.cdc=cotador_de_caza
+    def inanicion_desidratacion(self):
+        return super().inanicion_desidratacion()
+    def death(self):
+        return super().death()
+    def reproduction(self):
+        return super().reproduction()
     pass
 class planta_01 (organismo):
+    def __init__(self, vida, daño, sed, movimiento, estado, genero, posicion):
+        super().__init__(vida, daño, sed, movimiento, estado, genero, posicion)
+    def desidratacion(self):
+        if super().water == 0:
+            super().hp = super().hp - 1
+    def death(self):
+        return super().death()
     pass
 
 class ambiente:
