e5e58d7d540f0acbad16b395a2dcde7b5f8c364b
Cristian0Caceres <134634536+Cristian0Caceres@users.noreply.github.com>
2023-11-18 22:10:07 -0300

    actualizacion de clases y adicion de metodos
    se agregaron mas caracteristicas a algunas de las clases ademas de modificar los metodos de las clases

--- python.py
+++ python.py
@@ -2,17 +2,19 @@ import pygame as py
 import numpy  as np
 import time   as ti 
 import rxpy   as rp
+import random as ra 
 
 class organismo:
-    def __init__(self,vida,daño,energia,sed,movimiento,estado,genero,posicion):
+    def __init__(self,vida,daño,energia,sed,movimiento,estado,genero,posicion,dieta):
         self.hp = vida
         self.dmg = daño
-        self.enrg =energia
-        self.water =sed
-        self.move =movimiento
-        self.estate=estado
-        self.gender=genero
-        self.post=posicion
+        self.enrg = energia
+        self.water = sed
+        self.move = movimiento
+        self.estate = estado
+        self.gender = genero
+        self.post = posicion
+        self.diet = dieta
 
     def inanicion_desidratacion(self):
         if self.enrg < 100:
@@ -26,41 +28,15 @@ class organismo:
 
     def reproduction(self):
         pass #nose como continuar este codigo lo principal seria comprobar si 2 organismos de una mmisma
-           #especies estan presentes cerca de si y son del genero opuesto se reproduscan creando otro
-           #ser de la misma especie pero con los atrivutos reducidos por unos 3 ciclos aproximadamente
-           #ademas de que por esos ciclos no se pueda reproducir y tanpoco sus padres
-class carnivoro (organismo):
-    def __init__(self, vida, daño, energia, sed, movimiento, estado, genero, posicion, contador_de_caza):
-        super().__init__(vida, daño, energia, sed, movimiento, estado, genero, posicion)
-        self.cdc=contador_de_caza
-    def inanicion_desidratacion(self):
-        return super().inanicion_desidratacion()
-    def death(self):
-        return super().death()
-    def reproduction(self):
-        return super().reproduction()
-class hervivoro (organismo):
-    def __init__(self, vida, daño, energia, sed, movimiento, estado, genero, posicion):
-        super().__init__(vida, daño, energia, sed, movimiento, estado, genero, posicion)
-    def inanicion_desidratacion(self):
-        return super().inanicion_desidratacion()
-    def death(self):
-        return super().death()
-    def reproduction(self):
-        return super().reproduction()
-class omnivoro (organismo):
-    def __init__(self, vida, daño, energia, sed, movimiento, estado, genero, posicion, cotador_de_caza):
-        super().__init__(vida, daño, energia, sed, movimiento, estado, genero, posicion)
-        self.cdc=cotador_de_caza
-    def inanicion_desidratacion(self):
-        return super().inanicion_desidratacion()
-    def death(self):
-        return super().death()
-    def reproduction(self):
-        return super().reproduction()
+            #especies estan presentes cerca de si y son del genero opuesto se reproduscan creando otro
+            #ser de la misma especie pero con los atrivutos reducidos por unos 3 ciclos aproximadamente
+            #ademas de que por esos ciclos no se pueda reproducir y tanpoco sus padres
+class animal(organismo):
+    def __init__(self, vida, daño, energia, sed, movimiento, estado, genero, posicion, dieta):
+        super().__init__(vida, daño, energia, sed, movimiento, estado, genero, posicion, dieta)
 class planta_01 (organismo):
-    def __init__(self, vida, daño, sed, movimiento, estado, genero, posicion):
-        super().__init__(vida, daño, sed, movimiento, estado, genero, posicion)
+    def __init__(self, vida, daño, energia, sed, movimiento, estado, genero, posicion, dieta):
+        super().__init__(vida, daño, energia, sed, movimiento, estado, genero, posicion, dieta)
     def desidratacion(self):
         if super().water == 0:
             super().hp = super().hp - 1
@@ -68,13 +44,25 @@ class planta_01 (organismo):
         return super().death()
 
 class ambiente:
-    def __init__(self,agua,fertilidad,temperatura,humedad,condiciones_meteorologicas,sotenibilidad):
+    def __init__(self,agua,fertilidad,temperatura,humedad,condiciones_meteorologicas,sotenibilidad,tipo):
         self.h2o   = agua
         self.fert  = fertilidad
         self.temp  = temperatura
         self.hume  = humedad
         self.cm    = condiciones_meteorologicas
         self.soste = sotenibilidad
+        self.type = tipo
+    def humedads(self):
+        if self.h2o < 100:
+            self.hume = self.hume-1
+
+    def sequia(self):
+        if self.h2o < 1:
+            self.h2o == 0
+            self.fert = self.fert - 1
+            self.hume = 0
+            self.temp = self.temp + 1
+            self.type = "arido"
 
 #-------------------------
 # Creacion de la pantalla
