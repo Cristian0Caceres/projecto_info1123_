3e84e3330b101aab24e4faec67a8d674ab868b5f
Cristian0Caceres <134634536+Cristian0Caceres@users.noreply.github.com>
2023-11-25 20:35:20 -0300

    animales mas mapa
    okey ahora se crean los animales y los mapas pero ninguno interactúa con nadie

--- python - copia.py
+++ python - copia.py
@@ -1,62 +1,123 @@
 import pygame
 import random
+import matplotlib.pyplot as plt
 
-# Dimensiones de la ventana
-ANCHO = 700
-ALTO = 700
+# Definir la clase Organismo
+class Organismo:
+    def __init__(self, vida, daño, energia, sed, movimiento, estado, genero, posicionx, posiciony, dieta):
+        self.hp = vida
+        self.dmg = daño
+        self.enrg = energia
+        self.water = sed
+        self.move = movimiento
+        self.estate = estado
+        self.gender = genero
+        self.postx = posicionx
+        self.posty = posiciony
+        self.diet = dieta
+        self.repcont = 0
 
-# Tamaño de las casillas
-TAM_CASILLA = ANCHO // 5
+    def inanicion_desidratacion(self):
+        if self.enrg < 100:
+            self.hp = self.hp - 1
+        if self.water < 100:
+            self.hp = self.hp - 1
 
-# Tipos de ambiente
-TIPOS_AMBIENTE = ["arido", "humedo", "templado", "frio", "caluroso"]
+    def death(self):
+        if self.hp < 1:
+            self.estate = "Muerto"
 
-class Ambiente:
-    def __init__(self, agua, fertilidad, temperatura, humedad, condiciones_meteorologicas, sostenibilidad, tipo):
-        self.h2o = agua
-        self.fert = fertilidad
-        self.temp = temperatura
-        self.hume = humedad
-        self.cm = condiciones_meteorologicas
-        self.soste = sostenibilidad
-        self.type = tipo
+    def reproduction(self):
+        pass
 
-def main():
-    pygame.init()
-    pantalla = pygame.display.set_mode((ANCHO, ALTO))
+    def max_move(self):
+        if self.postx > 25:
+            self.postx = 25
+        if self.posty > 25:
+            self.posty = 25
 
-    # Generar el mapa
-    mapa = []
-    for i in range(5):
-        fila = []
-        for j in range(5):
-            h2o = random.randint(0, 100)
-            fertilidad = random.randint(0, 100)
-            temperatura = random.randint(-30, 50)
-            humedad = random.randint(0, 100)
-            condiciones_meteorologicas = random.choice(["soleado", "nublado", "lluvioso", "nevado"])
-            sostenibilidad = random.randint(0, 100)
-            tipo = random.choice(TIPOS_AMBIENTE)
-            fila.append(Ambiente(agua, fertilidad, temperatura, humedad, condiciones_meteorologicas, sostenibilidad, tipo))
-        mapa.append(fila)
+# Definir la clase Animal que hereda de Organismo
+class Animal(Organismo, pygame.sprite.Sprite):
+    def __init__(self, color, vida, daño, energia, sed, movimiento, estado, genero, posicionx, posiciony, dieta):
+        Organismo.__init__(self, vida, daño, energia, sed, movimiento, estado, genero, posicionx, posiciony, dieta)
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([10, 10])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(700)
+        self.rect.y = random.randrange(700)
 
-    # Dibujar el mapa
-    for i in range(len(mapa)):
-        for j in range(len(mapa[i])):
-            color = (0, mapa[i][j].fert, mapa[i][j].h2o)  # Use fertility and water as color components
-            pygame.draw.rect(pantalla, color, pygame.Rect(i*TAM_CASILLA, j*TAM_CASILLA, TAM_CASILLA, TAM_CASILLA))
+    def combat(self, target):
+        if self.estate == "Vivo" and target.estate == "Vivo":
+            target.hp -= self.dmg
+            if target.hp <= 0:
+                target.death()
 
-    # Bucle principal
-    ejecutando = True
-    while ejecutando:
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT:
-                ejecutando = False
+    def update(self):
+        self.rect.x += random.choice([-20, 20])
+        self.rect.y += random.choice([-20, 20])
+        if self.rect.x < 0 or self.rect.x > 690:
+            self.rect.x = random.randrange(700)
+        if self.rect.y < 0 or self.rect.y > 690:
+            self.rect.y = random.randrange(700)
 
-        pygame.display.flip()
+# Inicializar Pygame
+pygame.init()
 
-    pygame.quit()
+# Crear pantalla
+screen = pygame.display.set_mode([700, 700])
 
-if __name__ == "__main__":
-    main()
+# Crear grupo de sprites
+all_sprites = pygame.sprite.Group()
 
+# Crear 10 animales de colores aleatorios
+for i in range(10):
+    color = (random.randrange(256), random.randrange(256), random.randrange(256))
+    vida = 100
+    daño = 10 if i < 5 else 0  # Los primeros 5 animales son agresivos
+    energia = 100
+    sed = 100
+    movimiento = 1
+    estado = "Vivo"
+    genero = "Macho" if i % 2 == 0 else "Hembra"
+    posicionx = random.randrange(700)
+    posiciony = random.randrange(700)
+    dieta = "Carnívoro" if i < 5 else "Herbívoro"
+    animal = Animal(color, vida, daño, energia, sed, movimiento, estado, genero, posicionx, posiciony, dieta)
+    all_sprites.add(animal)
+
+# Crear un objeto Clock
+clock = pygame.time.Clock()
+
+# Bucle principal del juego
+running = True
+dead_animals = 0
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    all_sprites.update()
+
+    # Contar el número de animales muertos
+    for animal in all_sprites:
+        if animal.estate == "Muerto":
+            dead_animals += 1
+
+    # Generar un gráfico cuando el número de animales muertos llega a 5
+    if dead_animals == 5:
+        plt.figure(figsize=(5, 5))
+        plt.bar(["Vivos", "Muertos"], [10 - dead_animals, dead_animals])
+        plt.title("Número de animales vivos y muertos")
+        plt.show()
+        break
+
+    screen.fill((255, 255, 255))
+    all_sprites.draw(screen)
+
+    pygame.display.flip()
+
+    # Limitar la tasa de fotogramas a 5 FPS
+    clock.tick(5)
+
+pygame.quit()
--- python.py
+++ python.py
@@ -3,20 +3,24 @@
 #-------------------------
 import pygame as py 
 import numpy  as np
-import time   as ti
 import rxpy   as rp
 import random as ra
 #-------------------------
 #CONSTANTES
 #-------------------------
+
 ancho, largo = 700 , 700 ; ncx,ncy = 5,5
 dimCW= ancho / ncx ; dimCH= largo / ncy
-posibles_ambientes=["arido", "humedo", "templado", "frio", "caluroso"]
-ejecutando = True
+posibles_ambientes = ["arido", "humedo", "templado", "frio", "caluroso"]
+posibles_estados = ["vivo","muelto","cazando","bebiendo","reproduciendoce","diambulando",]
+posibles_generos = ["macho","hembra","Planta"] ; running = True
+posibles_dietas  = ["carnivoro","herviro"]; all_sprites = py.sprite.Group()
+mapa = [] ; clock = py.time.Clock()
 
 #-------------------------
 #CLASES
 #-------------------------
+
 class organismo:
     def __init__(self,vida,daño,energia,sed,movimiento,estado,genero,posicionx,posiciony,dieta):
         self.hp      =       vida
@@ -50,9 +54,24 @@ class organismo:
         if self.posty > 25:
             self.posty = 25
 
-class animal(organismo):
-    def __init__(self, vida, daño, energia, sed, movimiento, estado, genero, posicionx, posiciony, dieta):
+class Animal(organismo, py.sprite.Sprite):
+    def __init__(self, vida, daño, energia, sed, movimiento, estado, genero, posicionx, posiciony, dieta,colour):
         super().__init__(vida, daño, energia, sed, movimiento, estado, genero, posicionx, posiciony, dieta)
+        py.sprite.Sprite.__init__(self)
+        self.image = py.Surface([10, 10])
+        self.image.fill(colour)
+        self.rect = self.image.get_rect()
+        self.rect.x = ra.randrange(700)
+        self.rect.y = ra.randrange(700)
+
+    def update(self):
+        self.rect.x += ra.choice([-20, 20])
+        self.rect.y += ra.choice([-20, 20])
+        if self.rect.x < 0 or self.rect.x > 690:
+            self.rect.x = ra.randrange(700)
+        if self.rect.y < 0 or self.rect.y > 690:
+            self.rect.y = ra.randrange(700)
+
     def inanicion_desidratacion(self):
         return super().inanicion_desidratacion()
     def death(self):
@@ -90,8 +109,22 @@ class ambiente:
             self.temp = self.temp + 1
             self.type = "arido"
     def water(self):
+        liquido= self.h2o
+        rehidratacion=ra.randint in range (0, 4)
         if self.hume > 33 :
-            self.h2o=self.h2o + ra.randint in range (0, 4)
+            self.h2o=liquido + rehidratacion
+
+#--------------------------
+#Coliciones
+#--------------------------
+
+
+
+#-------------------------
+#animales
+#-------------------------
+
+
 
 #-------------------------
 #FUNCIONES
@@ -112,19 +145,28 @@ def Img_Init():
     aImg.append(Load_Image('T03.png',False )) # montaña
     aImg.append(Load_Image('T04.png',False )) # ¿hielo?
     return aImg
+
 #-------------------------
 #MAIN
 #-------------------------
+py.init()
+
 def main(ancho,largo,mapa):
     pantalla= py.display.set_mode((ancho,largo))
     for i in range(len(mapa)):
         for j in range(len(mapa[i])):
-            color = (0, mapa[i][j].fert, mapa[i][j].h2o)  # Use fertility and water as color components
+            mapa[i][j].humedads()
+            mapa[i][j].sequia()
+            mapa[i][j].water()
+            color = (0, mapa[i][j].fert, mapa[i][j].h2o)
             py.draw.rect(pantalla, color, py.Rect(i*dimCW, j*dimCH, dimCW, dimCH))
+            all_sprites.draw(pantalla)
+
+
 #---------------------------
 #GENERACION DEL MAPA
 #---------------------------
-mapa = []
+
 for i in range(5):
     fila = []
     for j in range(5):
@@ -134,16 +176,40 @@ for i in range(5):
         tipo = ra.choice(posibles_ambientes)
         fila.append(ambiente(agua, fertilidad, temperatura, humedad, condiciones_meteorologicas, sostenibilidad, tipo))
     mapa.append(fila)
+
+for i in range(10):
+    colour = (ra.randrange(256), ra.randrange(256), ra.randrange(256))
+    vida = 100
+    daño = 10 if i < 5 else 0  # Los primeros 5 animales son agresivos
+    energia = 100
+    sed = 100
+    movimiento = 1
+    estado = "Vivo"
+    genero = "Macho" if i % 2 == 0 else "Hembra"
+    posicionx = ra.randrange(700)
+    posiciony = ra.randrange(700)
+    dieta = "Carnívoro" if i < 5 else "Herbívoro"
+    animal = Animal( vida, daño, energia, sed, movimiento, estado, genero, posicionx, posiciony, dieta,colour)
+    all_sprites.add(animal)
+
 #---------------------------------------------------------------------
 # Inicializa Superficie del Super Extra Mega Mapa.-
 #---------------------------------------------------------------------
+
 def Get_Surface(ancho,alto):
     return py.Surface((ancho,alto))
 
 #-------------------------
 # CICLO PRINCIPAL
 #-------------------------
-while ejecutando:
-    ti.sleep(0.1)
+
+while running:
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            running = False
+    all_sprites.update()
     main(largo,ancho,mapa)
+
     py.display.flip()
+    clock.tick(5)
+py.quit()
