0a3dce247e20e0c50506c933147fd9856b6fb3f1
Yaninna Alvarez <134446812+Yaninna137@users.noreply.github.com>
2023-11-28 10:12:17 -0300

    Matriz

--- Creacion de mapa diseño/25.py
+++ Creacion de mapa diseño/25.py
@@ -5,24 +5,55 @@ import numpy as np
 # Cargamos las imágenes
 def cargar_imagenes():
     imagenes = []
-    imagenes.append(pygame.image.load('25/f1.png'))
-    imagenes.append(pygame.image.load('25/f2.png'))
-    imagenes.append(pygame.image.load('25/f3.png'))
-    imagenes.append(pygame.image.load('25/f4.png'))
-    imagenes.append(pygame.image.load('25/f5.png'))
-    imagenes.append(pygame.image.load('25/f6.png'))
-    imagenes.append(pygame.image.load('25/f7.png'))
-    imagenes.append(pygame.image.load('25/f8.png'))
-    imagenes.append(pygame.image.load('25/f9.png'))
-    imagenes.append(pygame.image.load('25/f10.png'))
-    imagenes.append(pygame.image.load('25/f11.png'))
-    imagenes.append(pygame.image.load('25/f13.png'))
+    imagenes.append(pygame.image.load('25/f3.png')) # Verde claro 1
+    imagenes.append(pygame.image.load('25/f5.png')) # Verde claro 2
+    imagenes.append(pygame.image.load('25/f4.png')) # Verde oscuro
+
+    imagenes.append(pygame.image.load('25/f2.png')) # Naranjo
+    imagenes.append(pygame.image.load('25/f1.png')) # Rojo
+
+    imagenes.append(pygame.image.load('25/f11.png'))# Blanco
+    imagenes.append(pygame.image.load('25/f13.png')) # Crema
+    imagenes.append(pygame.image.load('25/f6.png')) # Gris
+    imagenes.append(pygame.image.load('25/f9.png')) # Celeste
+
+
+    imagenes.append(pygame.image.load('25/f7.png')) # Rosa
+    imagenes.append(pygame.image.load('25/f8.png')) # Purpura 1
+    imagenes.append(pygame.image.load('25/f10.png')) # Purpura 2
+
     return imagenes
 
 
 # Inicializamos Pygame
 pygame.init()
 
+matris = [
+    [3,3,3,3,8,8,8,8,8,8,8,8,8,8,8,3,3,3,3,3,3,3,0,0,0,0,0,0,0,0,0,0],
+    [3,3,3,8,8,8,8,8,8,8,8,8,8,8,8,8,3,3,3,3,3,3,3,0,0,0,0,0,0,0,0,0],
+    [3,3,8,8,8,8,8,8,8,8,8,8,8,8,8,8,3,3,3,3,3,3,3,3,3,3,3,0,0,0,0,0],
+    [3,8,8,8,8,4,8,8,8,8,8,8,8,8,8,8,8,3,3,3,3,3,3,3,3,3,0,0,0,0,0,0],
+    [8,8,8,4,4,4,4,7,8,8,8,8,8,8,8,8,8,8,3,3,3,3,3,3,3,0,0,0,0,0,0,0],
+    [8,8,8,4,4,4,4,7,8,8,8,8,8,8,8,8,8,8,8,3,3,3,3,3,0,0,0,0,0,0,0,0],
+    [8,8,8,7,4,4,4,7,8,8,8,8,8,8,8,8,8,8,8,8,8,3,3,0,0,0,0,0,0,0,0,0],
+    [8,8,8,8,7,4,4,7,8,8,8,8,8,8,8,8,8,8,8,8,3,3,3,0,0,0,0,0,0,0,0,0],
+    [8,8,8,8,7,7,7,7,8,8,8,8,8,8,8,8,8,8,8,3,3,3,0,0,0,8,0,0,0,0,0,0],
+    [8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,3,0,0,0,8,8,8,8,0,0,0,0],
+    [8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,0,0,8,8,8,1,8,8,8,8,8],
+    [8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,0,8,1,1,1,1,8,8,8,8],
+    [8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,1,1,1,1,1,1,1,1],
+    [8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,1,1,1,1,1,1,1,1,1,1,1,1],
+    [3,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,2,1,1,1,1,1,1,1,1,1,1,1,1],
+    [5,3,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1],
+    [5,5,3,8,8,8,8,8,8,8,8,8,8,8,8,8,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1],
+    [5,5,5,3,8,8,8,8,8,8,8,8,8,8,8,3,2,2,2,2,2,2,1,1,8,1,1,1,1,1,1,1],
+    [5,5,5,5,3,8,8,8,8,8,8,8,8,8,8,3,2,2,2,2,2,2,2,8,8,8,1,1,1,1,1,1],
+    [5,5,5,5,5,3,8,8,8,8,8,8,8,8,3,3,2,2,2,2,2,2,8,8,8,8,8,8,8,1,1,1],
+    [5,5,5,5,5,3,3,8,8,8,8,8,8,8,8,3,3,2,2,2,2,2,2,8,8,8,8,8,1,1,1,1],
+    [5,5,5,8,5,5,3,3,8,8,8,8,8,8,3,3,3,2,2,8,8,2,2,2,8,2,8,1,1,1,1,1],
+    [5,5,8,8,8,5,5,3,8,8,8,8,8,3,3,3,3,2,2,8,2,2,2,2,2,2,2,1,1,1,1,1],
+    [5,5,5,5,5,5,5,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1]
+]
 # Creamos la pantalla
 pantalla = pygame.display.set_mode((1000, 600))
 pygame.display.set_caption("Simulador de ecosistema")
@@ -30,66 +61,27 @@ pygame.display.set_caption("Simulador de ecosistema")
 
 # Cargamos las imágenes
 imagenes = cargar_imagenes()
-## Crear la matriz , Primera capa , es el mar 
+
 for i in range(24):
     for j in range(32):
-        pantalla.blit(imagenes[8], (j * 25, i * 25))
-#-----------------------------------------------
-# Primer: DECIERTO
-#-----------------------------------------------
-Decierto_1 = [[0,0,0,0,1,1,1,2,2,3],[0,1,2,3,0,1,2,0,1,0]]
-
-for x in range(2):
-    for y in range(len(Decierto_1[0])):
-        pantalla.blit(imagenes[1],(Decierto_1[0][y]*25,Decierto_1[1][y]*25))
-
-Decierto_2 = [[0,0,0,0,0,0,0,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,5,5,5,5,5,6,6,7,7,7,8,8,8,9],[15,16,17,18,19,20,21,16,17,18,19,20,21,22,16,17,18,19,20,21,22,23,24,25,26,17,18,19,20,21,22,23,24,25,18,19,20,21,22,23,24,19,20,21,22,23,21,22,20,21,22,21,19,20,20]]
-
-for x in range(2):
-    for y in range(len(Decierto_2[0])):
-        pantalla.blit(imagenes[1],(Decierto_2[1][y]*25,Decierto_2[0][y]*25))
-
-
-borde_inferior_decierto = [[14,15,16,17,18,19,19,19,20,20,20,20,21,21,21,21,22,22,22,22,23,23,23,23],[0,1,2,15,15,5,14,15,5,6,15,16,6,7,14,15,8,13,14,15,9,12,13,14]]
-for x in range(2):
-    for y in range(len(borde_inferior_decierto[0])):
-        pantalla.blit(imagenes[1],(borde_inferior_decierto[1][y]*25,borde_inferior_decierto[0][y]*25))
-
-## MAPA ANTARTICA:
-Antartica = ([0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,4,4,4,4,4,4,5,5,5,6,6,7,7,8],[15,16,17,18,19,20,21,22,23,16,17,18,19,20,21,22,23,17,18,19,20,21,23,17,18,19,20,18,19,20,21,22,23,21,22,23,22,23,22,23,23])
-for x in range(2):
-    for y in range(len(Antartica[0])):
-        pantalla.blit(imagenes[11],(Antartica[0][y]*25,Antartica[1][y]*25))
-
-## MAPA DE SELVA:
-Selva = [[15,16,16,16,16,16,16,16,17,17,17,17,17,17,17,17,17,18,18,18,18,18,18,18,19,19,19,19,19,19,19,19,19,20,20,20,20,20,20,20,20,20,21,21,21,21,21,21,21,21,22,22,22,22,22,23,23,23,24,24,25,25,25,26,26],[23,16,17,18,19,21,22,23,15,16,17,18,19,20,21,22,23,15,16,17,18,19,20,23,14,15,16,17,18,19,20,22,23,15,16,17,18,19,20,21,22,23,16,17,18,19,20,21,22,23,18,20,21,22,23,21,22,23,22,23,21,22,23,22,23]]
-for x in range(2):
-    for y in range(len(Selva[0])):
-        pantalla.blit(imagenes[2],(Selva[0][y]*25,Selva[1][y]*25))
-## MAPA DE BOSQUE:
-Bosque_S = [[0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,3,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,10,10,11],[22,23,24,25,26,27,28,29,30,31,23,24,25,26,27,28,29,30,31,28,29,30,31,27,28,29,30,31,26,27,28,29,30,31,25,26,27,28,29,30,31,24,25,26,27,28,29,30,31,23,24,25,26,27,28,29,30,31,23,24,25,26,27,28,29,30,31,22,23,24,26,27,21,22,23,28,29,30,31,21,22,22]]
-
-for x in range(2):
-    for y in range(len(Bosque_S[0])):
-        pantalla.blit(imagenes[4],(Bosque_S[1][y]*25,Bosque_S[0][y]*25))
-
-
-Bosque_I = [[20,20,21,21,21,22,22,22,22,22,23,23,23,23,23,24,24,24,24,24,24,25,25,25,25,25,25,25,26,26,26,26,26,26,26,26,26,27,27,27,27,27,27,27,27,27,27,27,28,28,28,28,28,28,28,28,28,28,28,29,29,29,29,29,29,29,29,29,29,29,29,30,30,30,30,30,30,30,30,30,30,30,30,31,31,31,31,31,31,31,31,31,31,31,31],[13,14,13,14,15,13,14,15,16,17,13,14,15,16,17,11,12,13,14,15,16,11,12,13,14,15,16,17,10,11,12,13,14,15,16,17,18,11,12,13,14,15,16,17,18,21,22,23,12,13,14,15,16,17,18,20,21,22,23,12,13,14,15,16,17,18,19,20,21,22,23,12,13,14,15,16,17,18,19,20,21,22,23,12,13,14,15,16,17,18,19,20,21,22,23]]
-for x in range(2):
-    for y in range(len(Bosque_I[0])):
-        pantalla.blit(imagenes[3],(Bosque_I[0][y]*25,Bosque_I[1][y]*25))
-
-
-# ## MAPA DE ISLA:
-Isla_S = [[3,3,4,4,4,5,5,5,5,5,6,6,6,6],[4,5,4,5,6,3,4,5,6,7,4,5,6,7]]
-for x in range(2):
-    for y in range(len(Isla_S[0])):
-        pantalla.blit(imagenes[0],(Isla_S[0][y]*25,Isla_S[1][y]*25))
-
-Isla_I = [[3,4,4,5,6,7,7,7,7],[6,7,8,8,8,4,5,6,7]]
-for x in range(2):
-    for y in range(len(Isla_I[0])):
-        pantalla.blit(imagenes[5],(Isla_I[0][y]*25,Isla_I[1][y]*25))
+        if matris[i][j] == 0:
+            pantalla.blit(imagenes[0], (j * 25, i * 25))
+        if matris[i][j] == 1:
+            pantalla.blit(imagenes[1], (j * 25, i * 25))
+        if matris[i][j] == 2:
+            pantalla.blit(imagenes[2], (j * 25, i * 25))
+        if matris[i][j] == 3:
+            pantalla.blit(imagenes[3], (j * 25, i * 25))
+        if matris[i][j] == 4:
+            pantalla.blit(imagenes[4], (j * 25, i * 25))
+        if matris[i][j] == 5:
+            pantalla.blit(imagenes[5], (j * 25, i * 25))
+        if matris[i][j] == 6:
+            pantalla.blit(imagenes[6], (j * 25, i * 25))
+        if matris[i][j] == 7:
+            pantalla.blit(imagenes[7], (j * 25, i * 25))
+        if matris[i][j] == 8:
+            pantalla.blit(imagenes[8], (j * 25, i * 25))
 # Actualizamos la pantalla
 pygame.display.update()
 
--- Creacion de mapa diseño/25CC.py
+++ Creacion de mapa diseño/25CC.py
@@ -1,99 +1,200 @@
-import pygame,sys
+# --------------------------------------------------
+#SIMULADOR DE ECOSISTEMA
+#------------------------------------------------------
+import pygame as py 
 from pygame.locals import *
-import numpy as np
+import numpy  as np
+import random as ra
+import time as ti
+import sys
+#-------------------------
+#   CONSTANTES
+#-------------------------
+
+ancho, largo = 700 , 700 ; 
+ncx,ncy = 5,5
+dimCW= ancho / ncx ; 
+dimCH= largo / ncy
+posibles_ambientes = ["arido", "humedo", "templado", "frio", "caluroso"]
+posibles_estados = ["vivo","muelto"]
+posibles_generos = ["macho","hembra","planti"] ; running = True
+posibles_dietas  = ["carnivoro","herviro",];
+all_sprites = py.sprite.Group()
+
+clock = py.time.Clock()
+ROWS, COLS = 14, 14;todos = py.sprite.Group()
+cuadrado_SIZE = ancho // ROWS;FPS = 60;MAX_HIJOS = 6;TIEMPO_REPRODUCCION = FPS * 3;MAX_ANIMALES = 15
+# -----------------------------------------------
+# INICIALIZAR PYgame
+# ------------------------------------------------
+py.init()
+pantalla = py.display.set_mode((1000, 600))
+py.display.set_caption("Simulador de ecosistema")
+
+class Organismo:
+    def __init__(self,vida,daño,energia,sed,estado,genero,posicionx,posiciony,dieta,color):
+        self.hp      =       vida
+        self.dmg     =       daño
+        self.enrg    =    energia
+        self.water   =        sed
+        self.estate  =     estado
+        self.gender  =     genero
+        self.postx   =  posicionx
+        self.posty   =  posiciony
+        self.diet    =      dieta
+        self.repcont =          0
+        self.color   =      color
+
+
+    def death(self):
+        if self.hp < 1:
+            self.estate = "Muerto"
+
+    def inanicion_desidratacion(self):
+        self.enrg  = int(self.enrg)  - 1
+        self.water = int(self.water) - 1
+        if self.enrg < 100:
+            self.hp = self.hp - 1
+        if self.water < 100:
+            self.hp = self.hp - 1
+class Animal(Organismo, py.sprite.Sprite):
+    def __init__(self, vida, daño, energia, sed, estado, genero,dieta, color, x, y,posicionx,posiciony):
+        super().__init__(vida, daño, energia, sed, estado, genero,dieta,color,posicionx,posiciony)
+        py.sprite.Sprite.__init__(self)
+        self.image = py.Surface([10, 10])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.hijos = []
+        self.tiempo_reproduccion = 0
+
+    def inanicion_desidratacion(self):
+        return super().inanicion_desidratacion()
+    def death(self):
+        return super().death()
+    def reproduction (self, otro, todos):
+        if (self.color == otro.color and otro not in self.hijos and self not in otro.hijos and
+            self.tiempo_reproduccion >= TIEMPO_REPRODUCCION and len(self.hijos) < MAX_HIJOS and
+            len([x for x in todos if x.color == self.color]) < MAX_ANIMALES):
+            hijo = Animal(self.hp,self.dmg,self.enrg,self.water,self.estate,self.gender,self.diet,self.color, self.rect.x, self.rect.y,self.postx,self.posty)
+            self.hijos.append(hijo)
+            otro.hijos.append(hijo)
+            self.tiempo_reproduccion = 0
+            return hijo
+        return None
+
+    def mover(self):
+        self.rect.x += ra.randint(-1, 1)
+        self.rect.y += ra.randint(-1, 1)
+
+
+        if self.color == (0, 0, 255):
+            hit_list = py.sprite.spritecollide(self, todos, False)
+            for hit in hit_list:
+                if isinstance(hit, Planta):
+
+                    self.tiempo_reproduccion -= 10
+                    todos.remove(hit)
+
+    def actualizar(self):
+        self.tiempo_reproduccion += 1
+
+    def beber_agua(self, grid):
+        celda_x = int(self.rect.x // dimCW)
+        celda_y = int(self.rect.y // dimCH)
+
+        if grid[celda_y][celda_x] == 0:
+            self.agua = min(100, self.agua + 10)
+            self.sed = max(0, self.sed - 10)
+
 
 # Cargamos las imágenes
 def cargar_imagenes():
     imagenes = []
-    imagenes.append(pygame.image.load('25CC/f1.jpg'))
-    imagenes.append(pygame.image.load('25CC/f2.jpg'))
-    imagenes.append(pygame.image.load('25CC/f3.jpg'))
-    imagenes.append(pygame.image.load('25CC/f4.jpg'))
-    imagenes.append(pygame.image.load('25CC/f5.jpg'))
-    imagenes.append(pygame.image.load('25CC/f6.jpg'))
-    imagenes.append(pygame.image.load('25CC/f7.jpg'))
-    imagenes.append(pygame.image.load('25CC/f8.jpg'))
-    imagenes.append(pygame.image.load('25CC/f9.jpg'))
+    imagenes.append(py.image.load('25CC/f5.jpg')) # VERDE  [0]
+    imagenes.append(py.image.load('25CC/f7.jpg')) # NARANJA [1]
+    imagenes.append(py.image.load('25CC/f4.jpg')) # ROJO [2]
+    imagenes.append(py.image.load('25CC/f3.jpg')) # MORADO 1[3]
+    imagenes.append(py.image.load('25CC/f6.jpg')) # AMARILLO [4]
+    imagenes.append(py.image.load('25CC/f1.jpg')) # GRIS [5]
+    imagenes.append(py.image.load('25CC/f2.jpg')) # CELESTE 1 [6] 
+    imagenes.append(py.image.load('25CC/f9.jpg')) # CELESTE 2 [7]
+    imagenes.append(py.image.load('25CC/f8.jpg')) # MORADO 2[8]
     return imagenes
 
 
-# Inicializamos Pygame
-pygame.init()
+def MATRIS_SIMULADOR():
+    imagenes = cargar_imagenes()
+    matris = [
+        [3,3,3,3,7,7,7,7,7,7,7,7,7,7,7,1,1,1,1,1,1,1,4,4,4,4,4,4,4,4,4,4],
+        [3,3,3,7,7,7,7,7,7,7,7,7,7,7,7,7,1,1,1,1,1,1,1,4,4,4,4,4,4,4,4,4],
+        [3,3,7,7,7,7,7,7,7,7,7,7,7,7,7,7,1,1,1,1,1,1,1,1,1,1,1,4,4,4,4,4],
+        [3,7,7,7,7,0,7,7,7,7,7,7,7,7,7,7,7,1,1,1,1,1,1,1,1,1,4,4,4,4,4,4],
+        [7,7,7,0,0,0,0,2,7,7,7,7,7,7,7,7,7,7,1,1,1,1,1,1,1,4,4,4,4,4,4,4],
+        [7,7,7,0,0,0,0,2,7,7,7,7,7,7,7,7,7,7,7,1,1,1,1,1,4,4,4,4,4,4,4,4],
+        [7,7,7,2,0,0,0,2,7,7,7,7,7,7,7,7,7,7,7,7,7,1,1,4,4,4,4,4,4,4,4,4],
+        [7,7,7,7,2,0,0,2,7,7,7,7,7,7,7,7,7,7,7,1,1,1,1,4,4,4,4,4,4,4,4,4],
+        [7,7,7,7,2,2,2,7,7,7,7,7,7,7,7,7,7,7,1,1,1,4,4,4,7,4,4,4,4,4,4,4],
+        [7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,1,4,4,4,7,7,7,7,4,4,4,4,4],
+        [7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,4,4,7,7,7,0,7,7,7,7,7,7],
+        [7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,4,7,0,0,0,0,7,7,7,7,7],
+        [7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,0,0,0,0,0,0,0,0,0],
+        [7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [3,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,2,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [3,3,7,7,7,7,7,7,7,7,7,7,7,7,7,7,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0],
+        [3,3,3,7,7,7,7,7,7,7,7,7,7,7,7,2,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0],
+        [3,3,3,3,7,7,7,7,7,7,7,7,7,7,3,2,2,2,2,2,2,0,0,7,0,0,0,0,0,0,0,0],
+        [3,3,3,3,3,7,7,7,7,7,7,7,7,7,3,2,2,2,2,2,2,2,7,7,7,0,0,0,0,0,0,0],
+        [3,3,3,3,3,3,7,7,7,7,7,7,7,3,3,2,2,2,2,2,2,7,7,7,7,7,7,7,7,0,0,0],
+        [3,3,3,7,3,3,3,7,7,7,7,7,7,7,3,3,2,2,2,2,2,2,7,7,7,7,7,7,0,0,0,0],
+        [3,3,7,7,3,3,3,7,7,7,7,7,7,3,3,3,2,2,7,7,2,2,2,2,7,2,7,0,0,0,0,0],
+        [3,3,3,3,3,3,3,7,7,7,7,7,3,3,3,3,2,2,7,2,2,2,2,2,2,2,2,0,0,0,0,0],
+        [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,0,0,0,0,0]
+    ]
+    for i in range(24):
+        for j in range(32):
+            if matris[i][j] == 0:
+                pantalla.blit(imagenes[0], (j * 25, i * 25))
+            if matris[i][j] == 1:
+                pantalla.blit(imagenes[1], (j * 25, i * 25))
+            if matris[i][j] == 2:
+                pantalla.blit(imagenes[2], (j * 25, i * 25))
+            if matris[i][j] == 3:
+                pantalla.blit(imagenes[3], (j * 25, i * 25))
+            if matris[i][j] == 4:
+                pantalla.blit(imagenes[4], (j * 25, i * 25))
+            if matris[i][j] == 5:
+                pantalla.blit(imagenes[5], (j * 25, i * 25))
+            if matris[i][j] == 6:
+                pantalla.blit(imagenes[6], (j * 25, i * 25))
+            if matris[i][j] == 7:
+                pantalla.blit(imagenes[7], (j * 25, i * 25))
+            if matris[i][j] == 8:
+                pantalla.blit(imagenes[8], (j * 25, i * 25))
 
-# Creamos la pantalla
-pantalla = pygame.display.set_mode((1000, 600))
-pygame.display.set_caption("Simulador de ecosistema")
-
-
-# Cargamos las imágenes
-imagenes = cargar_imagenes()
-## Crear la matriz , Primera capa , es el mar 
-for i in range(24):
-    for j in range(32):
-        pantalla.blit(imagenes[8], (j * 25, i * 25))
-#-----------------------------------------------
-# Primer: DECIERTO
-#-----------------------------------------------
-Decierto_1 = [[0,0,0,0,1,1,1,2,2,3],[0,1,2,3,0,1,2,0,1,0]]
-
-for x in range(2):
-    for y in range(len(Decierto_1[0])):
-        pantalla.blit(imagenes[0],(Decierto_1[0][y]*25,Decierto_1[1][y]*25))
-
-Decierto_2 = [[0,0,0,0,0,0,0,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,5,5,5,5,5,6,6,7,7,7,8,8,8,9],[15,16,17,18,19,20,21,16,17,18,19,20,21,22,16,17,18,19,20,21,22,23,24,25,26,17,18,19,20,21,22,23,24,25,18,19,20,21,22,23,24,19,20,21,22,23,21,22,20,21,22,21,19,20,20]]
-
-for x in range(2):
-    for y in range(len(Decierto_2[0])):
-        pantalla.blit(imagenes[6],(Decierto_2[1][y]*25,Decierto_2[0][y]*25))
-
-
-borde_inferior_decierto = [[14,15,16,17,18,19,19,19,20,20,20,20,21,21,21,21,22,22,22,22,23,23,23,23],[0,1,2,15,15,5,14,15,5,6,15,16,6,7,14,15,8,13,14,15,9,12,13,14]]
-for x in range(2):
-    for y in range(len(borde_inferior_decierto[0])):
-        pantalla.blit(imagenes[0],(borde_inferior_decierto[1][y]*25,borde_inferior_decierto[0][y]*25))
-
-## MAPA ANTARTICA:
-Antartica = ([0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,4,4,4,4,4,4,5,5,5,6,6,7,7,8],[15,16,17,18,19,20,21,22,23,16,17,18,19,20,21,22,23,17,18,19,20,21,23,17,18,19,20,18,19,20,21,22,23,21,22,23,22,23,22,23,23])
-for x in range(2):
-    for y in range(len(Antartica[0])):
-        pantalla.blit(imagenes[3],(Antartica[0][y]*25,Antartica[1][y]*25))
-
-## MAPA DE SELVA:
-Selva = [[15,16,16,16,16,16,16,16,17,17,17,17,17,17,17,17,17,18,18,18,18,18,18,18,19,19,19,19,19,19,19,19,19,20,20,20,20,20,20,20,20,20,21,21,21,21,21,21,21,21,22,22,22,22,22,23,23,23,24,24,25,25,25,26,26],[23,16,17,18,19,21,22,23,15,16,17,18,19,20,21,22,23,15,16,17,18,19,20,23,14,15,16,17,18,19,20,22,23,15,16,17,18,19,20,21,22,23,16,17,18,19,20,21,22,23,18,20,21,22,23,21,22,23,22,23,21,22,23,22,23]]
-for x in range(2):
-    for y in range(len(Selva[0])):
-        pantalla.blit(imagenes[2],(Selva[0][y]*25,Selva[1][y]*25))
-## MAPA DE BOSQUE:
-Bosque_S = [[0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,3,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,10,10,11],[22,23,24,25,26,27,28,29,30,31,23,24,25,26,27,28,29,30,31,28,29,30,31,27,28,29,30,31,26,27,28,29,30,31,25,26,27,28,29,30,31,24,25,26,27,28,29,30,31,23,24,25,26,27,28,29,30,31,23,24,25,26,27,28,29,30,31,22,23,24,26,27,21,22,23,28,29,30,31,21,22,22]]
-
-for x in range(2):
-    for y in range(len(Bosque_S[0])):
-        pantalla.blit(imagenes[5],(Bosque_S[1][y]*25,Bosque_S[0][y]*25))
-
-
-Bosque_I = [[20,20,21,21,21,22,22,22,22,22,23,23,23,23,23,24,24,24,24,24,24,25,25,25,25,25,25,25,26,26,26,26,26,26,26,26,26,27,27,27,27,27,27,27,27,27,27,27,28,28,28,28,28,28,28,28,28,28,28,29,29,29,29,29,29,29,29,29,29,29,29,30,30,30,30,30,30,30,30,30,30,30,30,31,31,31,31,31,31,31,31,31,31,31,31],[13,14,13,14,15,13,14,15,16,17,13,14,15,16,17,11,12,13,14,15,16,11,12,13,14,15,16,17,10,11,12,13,14,15,16,17,18,11,12,13,14,15,16,17,18,21,22,23,12,13,14,15,16,17,18,20,21,22,23,12,13,14,15,16,17,18,19,20,21,22,23,12,13,14,15,16,17,18,19,20,21,22,23,12,13,14,15,16,17,18,19,20,21,22,23]]
-for x in range(2):
-    for y in range(len(Bosque_I[0])):
-        pantalla.blit(imagenes[4],(Bosque_I[0][y]*25,Bosque_I[1][y]*25))
-
-
-# ## MAPA DE ISLA:
-Isla_S = [[3,3,4,4,4,5,5,5,5,5,6,6,6,6],[4,5,4,5,6,3,4,5,6,7,4,5,6,7]]
-for x in range(2):
-    for y in range(len(Isla_S[0])):
-        pantalla.blit(imagenes[4],(Isla_S[0][y]*25,Isla_S[1][y]*25))
-
-Isla_I = [[3,4,4,5,6,7,7,7,7],[6,7,8,8,8,4,5,6,7]]
-for x in range(2):
-    for y in range(len(Isla_I[0])):
-        pantalla.blit(imagenes[3],(Isla_I[0][y]*25,Isla_I[1][y]*25))
-# Actualizamos la pantalla
-pygame.display.update()
+MATRIS_SIMULADOR()
+py.display.update()
 
 # Mantenemos la ventana abierta hasta que el usuario la cierre
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in py.event.get():
+        if event.type == py.QUIT:
             sys.exit()
-            pygame.quit()
-            quit()
+    if event.type == py.KEYDOWN:
+        if event.key == py.K_m:
+            py.draw.rect()
+    #     if event.key == py.K_RIGHT:
+    #         speend_x = 3
+    # if event.type == py.KEYUP:  
+    #     if event.key == py.K_LEFT:  
+    #         speend_x = 0
+    #     if event.key == py.K_RIGHT:
+    #         speend_x = 0
+
+    MATRIS_SIMULADOR()
+
+
+    py.display.update()
+    py.display.flip()
+    clock.tick(150)  # Tiempo de como se vera la animación 
